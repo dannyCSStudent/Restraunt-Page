68a31e6fb83bcf543406d42aaa6e30b6c4862185
Dan Villegas <danv8605@gmail.com>
2023-01-14 10:20:23 -0600

    First enrty

---
+++ src/home.js
@@ -0,0 +1,9 @@
+
+
+const home = () => {
+    const h = document.createElement('div');
+    h.classList.add('home');
+    h.textContent = 'Have you ever been slapped in the face? Well your taste buds are about to get bitch slapped.';
+    return h;
+} 
+export default home;
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import './styles.css';
+import bg from './us.png';
+import home from './home';
+
+
+const content = document.getElementById('content');
+
+function loadPage () {
+    console.log(home().textContent + ' kk');
+    const b = new Image();
+    b.src = bg;
+
+    const banner = document.createElement('div');
+    const navBar = document.createElement('div');
+    const btn_home = document.createElement('button');
+    const btn_menu = document.createElement('button');
+    const btn_contact = document.createElement('button');
+    const body_container = document.createElement('div');
+    const body_content = document.createElement('div');
+
+    banner.classList.add('banner'); 
+    navBar.classList.add('nav-bar');
+    body_container.classList.add('body-container');
+    body_content.classList.add('body-content');
+
+
+    banner.textContent = "Ary's Pastries"; 
+    btn_home.textContent = 'HOME';
+    btn_menu.textContent = 'MENU';
+    btn_contact.textContent = 'CONTACT';
+
+    navBar.appendChild(btn_home);
+    navBar.appendChild(btn_menu);
+    navBar.appendChild(btn_contact);
+    body_container.appendChild(body_content);
+
+    content.appendChild(navBar);
+    content.appendChild(banner);
+    content.appendChild(body_container);
+}
+loadPage();
